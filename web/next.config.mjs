26d6514d0871000945d10cd891be837e1aabdffd
hwangsungyo <69719583+gdkds12@users.noreply.github.com>
2026-01-07 22:22:46 +0900

    Update next.config.mjs

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    serverExternalPackages: ['@google-cloud/run', '@google-cloud/storage', 'google-auth-library'],
 };
 
 export default nextConfig;
